76e8215d650e0b1f94b268bb61cdba252f9e67f9
drumzminister <ishukpong418@gmail.com>
2019-04-15 21:59:47 +0100

    fixed contact page mail limnks

--- resources/views/navigation/contact.blade.php
+++ resources/views/navigation/contact.blade.php
@@ -22,15 +22,16 @@
                 <p class="text-secondaryy mt-4 pr-3">
                     We operate the industry on trust This can only be achieved through communication
                     and experienced support from the first contact through to subsequent ones.
-                    sapiente soluta, tempore ut voluptas.
+                    We are here to make every contact a memorable one with swift and on the point response.
                 </p>
                 <br>
                 <h3 class="text-secondaryy">At Paylittle you always talk to a human!</h3>
                 <p class="text-secondaryy mt-4 pr-3">
-                    We operate the industry on trust This can only be achieved through communication
-                    and experienced support from the first contact through to subsequent ones.
-                    sapiente soluta, tempore ut voluptas.
+                    We operate the industry on trust This can only be achieved through communication and experienced support from the first contact
+                    through to subsequent ones. We are here to make every contact a memorable one with swift and on the point response.
                 </p>
+                a
+
             </div>
             <div class="col-1 border-right"></div>
             <div class="col-md-4 col-12 pl-5">
@@ -39,13 +40,24 @@
                 <br>
                 <h4 class="p-c font-weight-light mb-4">Direct Contact</h4>
                 <p class="text-secondaryy">Phone: +234(0)8123641748</p>
-                <p class="text-secondaryy">Email: info@farmsponsor.com.ng</p>
+                <p class="text-secondaryy">Email: <a href="mailto:info@farmsponsor.com.ng"> info@farmsponsor.com.ng </a></p>
                 <br>
                 <h4 class="p-c font-weight-light mb-4">Departments</h4>
-                <p class="text-secondaryy">info@farmsponsor.com.ng</p>
-                <p class="text-secondaryy">info@farmsponsor.com.ng</p>
-                <p class="text-secondaryy">info@farmsponsor.com.ng</p>
-                <p class="text-secondaryy">info@farmsponsor.com.ng</p>
+                <p class="text-secondaryy">
+                    <a href="mailto:admin@farmsponsor.com.ng">
+                        admin@farmsponsor.com.ng
+                    </a>
+                </p>
+                <p class="text-secondaryy">
+                    <a href="mailto:sponsorship@farmsponsor.com.ng">
+                        sponsorship@farmsponsor.com.ng
+                    </a>
+                </p>
+                <p class="text-secondaryy">
+                    <a href="mailto:techsupport@farmsponsor.com.ng">
+                        techsupport@farmsponsor.com.ng
+                    </a>
+                </p>
                 <br>
             </div>
         </div>
